d25c383f0f4d81b24f6e8fe22fe3b71683c633a4
efrabaez <e.german@hotmail.com>
2025-04-29 16:55:42 -0600

    Added MongoDB connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from './config/envs';
+import { MongoDataBase } from './data';
 import { AppRoutes } from './presentation/routes';
 import { Server } from './presentation/server';
 
@@ -8,8 +9,13 @@ import { Server } from './presentation/server';
 })();
 
 
-function main() {
+async function main() {
 
+  await MongoDataBase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_DB_NAME,
+  });
+  
   const server = new Server({
     port: envs.PORT,
     routes: AppRoutes.routes,
