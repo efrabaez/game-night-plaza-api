673dc0837c32e2e3eca5fed7b53b86a212fbbaf7
efrabaez <e.german@hotmail.com>
2025-06-05 16:36:48 -0600

    [FIX] agregate condition to middleware to avoid getting errors from no payload on body since change of express 4 to 5

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -8,32 +8,38 @@ export class AuthMiddleware {
 
 
   static async validateJWT( req: Request, res: Response, next: NextFunction ) {
+    /**
+     * req.body
+     * The req.body property returns undefined when the body has not been parsed. In Express 4, it returns {} by default.
+     * Making a validation on the middleware to send new message if there is no body
+     */
+    if (req.body === undefined) {
+      return void res.status(401).json({ data: [], message: 'Body payload was not sent' });
+    }
 
     const authorization = req.header('Authorization');
-    if( !authorization ) return void res.status(401).json({ message: 'No token provided' });
+    if( !authorization ) return void res.status(401).json({ data: [], message: 'No token provided' });
     if ( !authorization.startsWith('Bearer ') ) return void res.status(401).json({ message: 'Invalid Bearer token' });
 
     const token = authorization.split(' ').at(1) || '';
 
-
     try {
 
       const payload = await JwtAdapter.validateToken<{ id: string }>(token);
-      if ( !payload ) return void res.status(401).json({ message: 'Invalid token' })
+      if ( !payload ) return void res.status(401).json({ data: [], message: 'Invalid token' })
       
       const user = await UserModel.findById( payload.id );
-      if ( !user ) return void res.status(401).json({ message: 'Invalid token - user' });
-
+      if ( !user ) return void res.status(401).json({ data: [], message: 'Invalid token - user' });
       // todo: is mail validate?
-
       req.body.user = UserEntity.fromObject(user);
+      
 
       next();
 
     } catch (error) {
       
       console.log(error);
-      res.status(500).json({ message: 'Internal server error' });
+      res.status(500).json({ data: [], message: 'Internal server error' });
 
     }
     
